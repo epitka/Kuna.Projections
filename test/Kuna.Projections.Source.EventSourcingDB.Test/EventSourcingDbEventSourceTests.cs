9455a271d58d4110307a16e425e3e4f488d36ff5
epitka <epitka014@gmail.com>
2026-06-25 06:44:55 -0500

    test EventSourcingDB observe reconnect

--- test/Kuna.Projections.Source.EventSourcingDB.Test/EventSourcingDbEventSourceTests.cs
+++ test/Kuna.Projections.Source.EventSourcingDB.Test/EventSourcingDbEventSourceTests.cs
@@ -1,11 +1,14 @@
 using EventSourcingDb;
+using EventSourcingDb.Types;
 using FakeItEasy;
+using Kuna.Projections.Abstractions.Messages;
 using Kuna.Projections.Abstractions.Models;
 using Kuna.Projections.Abstractions.Services;
 using Kuna.Projections.Source.EventSourcingDB;
 using Microsoft.Extensions.Logging.Abstractions;
 using Shouldly;
 using Xunit;
+using EsdbEvent = EventSourcingDb.Types.Event;
 
 namespace Kuna.Projections.Source.EventSourcingDB.Test;
 
@@ -36,13 +39,69 @@ public class EventSourcingDbEventSourceTests
         source.ShouldNotBeNull();
     }
 
+    [Fact]
+    public async Task ReadAll_Should_Reopen_Observe_When_It_Completes_Normally()
+    {
+        var client = A.Fake<IClient>();
+        var observeCalls = 0;
+
+        A.CallTo(
+             () => client.ObserveEventsAsync(
+                 A<string>._,
+                 A<ObserveEventsOptions>._,
+                 A<CancellationToken>._))
+         .ReturnsLazily(
+             call =>
+             {
+                 var callNumber = Interlocked.Increment(ref observeCalls);
+                 var cancellationToken = call.GetArgument<CancellationToken>(2);
+                 return callNumber == 1
+                            ? CompleteImmediately()
+                            : WaitUntilCancelled(cancellationToken);
+             });
+
+        var headPositionReader = A.Fake<IHeadPositionReader>();
+        A.CallTo(() => headPositionReader.ReadHeadPositionAsync(A<CancellationToken>._))
+         .Returns("0");
+
+        var source = CreateSource(
+            EventVersionCheckStrategy.Monotonic,
+            client,
+            headPositionReader);
+
+        using var cts = new CancellationTokenSource(TimeSpan.FromSeconds(5));
+        await using var enumerator = source.ReadAll(new GlobalEventPosition("0"), cts.Token)
+                                           .GetAsyncEnumerator(cts.Token);
+
+        (await enumerator.MoveNextAsync()).ShouldBeTrue();
+        enumerator.Current.Event.ShouldBeOfType<ProjectionCaughtUpEvent>();
+
+        var next = enumerator.MoveNextAsync().AsTask();
+        await WaitUntilAsync(() => Volatile.Read(ref observeCalls) >= 2, cts.Token);
+
+        next.IsCompleted.ShouldBeFalse();
+        await cts.CancelAsync();
+        (await next).ShouldBeFalse();
+    }
+
     private static EventSourcingDbEventSource<TestModel> CreateSource(EventVersionCheckStrategy strategy)
     {
-        return new EventSourcingDbEventSource<TestModel>(
+        return CreateSource(
+            strategy,
             A.Fake<IClient>(),
+            A.Fake<IHeadPositionReader>());
+    }
+
+    private static EventSourcingDbEventSource<TestModel> CreateSource(
+        EventVersionCheckStrategy strategy,
+        IClient client,
+        IHeadPositionReader headPositionReader)
+    {
+        return new EventSourcingDbEventSource<TestModel>(
+            client,
             A.Fake<IEventEnvelopeFactory>(),
             new EventSourcingDbCheckpointSerializer(),
-            A.Fake<IHeadPositionReader>(),
+            headPositionReader,
             new EventSourcingDbSourceSettings(),
             new ProjectionSettings<TestModel>
             {
@@ -50,6 +109,27 @@ public class EventSourcingDbEventSourceTests
                 EventVersionCheckStrategy = strategy,
             },
             NullLogger<EventSourcingDbEventSource<TestModel>>.Instance);
+    }
+
+    private static async IAsyncEnumerable<EsdbEvent> CompleteImmediately()
+    {
+        await Task.CompletedTask;
+        yield break;
+    }
+
+    private static async IAsyncEnumerable<EsdbEvent> WaitUntilCancelled(
+        [System.Runtime.CompilerServices.EnumeratorCancellation] CancellationToken cancellationToken)
+    {
+        await Task.Delay(Timeout.InfiniteTimeSpan, cancellationToken);
+        yield break;
+    }
+
+    private static async Task WaitUntilAsync(Func<bool> condition, CancellationToken cancellationToken)
+    {
+        while (!condition())
+        {
+            await Task.Delay(TimeSpan.FromMilliseconds(10), cancellationToken);
+        }
     }
 
     private sealed class TestModel : Model
